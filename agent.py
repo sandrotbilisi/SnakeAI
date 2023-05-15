d4a9fa0657468ec5b411c46bafc302b7b13292de
sandrotbilisi <128144834+sandrotbilisi@users.noreply.github.com>
2023-05-16 01:26:20 +0400

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,17 @@
+import torch
+import random
+import numpy as np
+from collections import deque
+from snake_game import SnakeGameAI, Direction, Point
+
+MAX_MEMORY = 100_000
+BATCH_SIZE= 1000
+LR = 0.001
+
+class Agent:
+
+    def __init__(self):
+        pass
+    def get_state(self, game):
+        pass
+    
